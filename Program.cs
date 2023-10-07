aae73c0aa72e6dea148190242aa7b3ca192fd1d0
Tiago Marmitt <tiagomarmittt@gmail.com>
2023-10-07 15:57:52 -0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BankAdministration
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Everyone");
+        }
+    }    
+}
